67b2c6d0c4d88abcb6f8575da3ad53fa90670d93
Filipe Godinho Gomes <tqi_fgodinho@uolinc.com>
2023-11-04 22:33:15 -0300

    add build Client.java

--- ecommerce/src/main/java/com/wbrain/ecommerce/domain/Client.java
+++ ecommerce/src/main/java/com/wbrain/ecommerce/domain/Client.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Builder
 public class Client {
 
     @Id
