3a0cabb6e40266bacbdf41ca203c3acbbb0578db
Filipe Godinho Gomes <tqi_fgodinho@uolinc.com>
2023-11-04 22:33:31 -0300

    add build NotesRequest.java

--- ecommerce/src/main/java/com/wbrain/ecommerce/request/NotesRequest.java
+++ ecommerce/src/main/java/com/wbrain/ecommerce/request/NotesRequest.java
@@ -1,12 +1,14 @@
 package com.wbrain.ecommerce.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Data
+@Builder
 public class NotesRequest {
 
     private LocalDate data;
